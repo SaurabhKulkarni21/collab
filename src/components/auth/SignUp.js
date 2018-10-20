9c475b73a0ecd18549d884d1d34cc2183d743968
SaurabhKulkarni21 <kulkarnisaurabh95@gmail.com>
2018-10-19 22:50:26 -0400

    Signup functionality with storing the data in firestore.

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
 
 class SignUp extends Component {
 
@@ -13,7 +14,7 @@ class SignUp extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault()
-    console.log(this.state)
+    this.props.signUp(this.state);
     
   }
 
@@ -69,4 +70,10 @@ const mapStateToProps = (state) => {
         auth: state.firebase.auth
     }
 }
-export default connect(mapStateToProps)(SignUp)
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
