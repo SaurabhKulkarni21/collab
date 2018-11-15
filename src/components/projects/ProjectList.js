56e4880ec2864a7208837019bd0977a35b34cb48
SaurabhKulkarni21 <kulkarnisaurabh95@gmail.com>
2018-11-15 18:14:43 -0500

    Fixed the population of projects on the dashboard in the col style.

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -3,32 +3,51 @@ import ProjectSummary from './ProjectSummary'
 import Link from 'react-router-dom/Link';
 
 const drawDivs = (projects) => {
+   const projectList = projects ? projects : [];
+   console.log("This is firstProject", projectList[0]);
    const divList = [];
    const length = projects && projects.length;
-   console.log(length);
-   let count = length/2;
-   
-   for(var i = 0, pivot=0; i < count; i++, pivot=pivot+2)
-   
-   {
-        divList.push(
-        <div className = "row">
-            <div className = "col" style= {{width:"50%"}} id={projects && projects[pivot].id}>
-                <Link to ={'/projects/'+projects[pivot].id}  key={projects[pivot].id }>
-                <ProjectSummary project={projects && projects[pivot]}/>
-                </Link>
-            </div>
+   console.log("This is length",length);
+   let rowCount = Math.floor(length/2) + (length % 2); 
+   console.log("This is the row count", rowCount);
+   let pivot = 0;
 
-            <div className= "col" style= {{width:"50%"}} id={projects && projects[pivot+1].id}>
-                <Link to ={'/projects/'+projects[pivot+1].id}  key={projects[pivot+1].id }>
-                <ProjectSummary project={projects && projects[pivot+1]}/>
-                </Link>
-            </div>
-        </div>
-        
-         
+   for(var i=0; i<= rowCount-1; i++){
+       console.log("This is the current iteration", i);
+       console.log("This is the pivot", pivot);
+
+       if(i == rowCount-1 && (length % 2) !== 0){
+        console.log("Yes the row count is equal to the end")
+        divList.push(
+           <div className="row">
+               <div className = "col" style = {{width:"50%"}} id={projectList[pivot].id}>
+                    <Link to ={'/projects/'+projectList[pivot].id} key={projectList[pivot].id}>
+                    <ProjectSummary project={projectList[pivot]}></ProjectSummary>
+                    </Link>
+               </div>
+           </div>
         );
+       }
+       else{
+        
+        divList.push(
+            <div className= "row">
+                <div className = "col" style = {{width:"50%"}} id={projectList[pivot].id}>
+                    <Link to ={'/projects/'+projectList[pivot].id} key={projectList[pivot].id}>
+                    <ProjectSummary project={projectList[pivot]}></ProjectSummary>
+                    </Link>
+                </div>
+
+                <div className = "col" style = {{width:"50%"}} id={projectList[pivot+1].id}>
+                    <Link to ={'/projects/'+projectList[pivot+1].id} key ={projectList[pivot+1].id}>
+                    <ProjectSummary project={projectList[pivot+1]}></ProjectSummary>
+                    </Link>
+                </div>
+            </div>
+       );
+       }
        
+       pivot=pivot+2;
    }
 
    return divList
